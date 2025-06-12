74ae226b33877bbc3479f5fbbbae32636fdad57a
AkashItlogiko <akashsahanwu089@gmail.com>
2025-06-12 13:56:29 +0600

    ok

--- app/Http/Controllers/ArticalController.php
+++ app/Http/Controllers/ArticalController.php
@@ -7,10 +7,27 @@ use Illuminate\Http\Request;
 class ArticalController extends Controller
 {
     //
-    function index($articalId){
+    function index($articalId = 1){
+        // if($articalId == null){
+        //     return "Artical ID IS: 0";
+        // }else{
+        //     return "Artical ID IS: " .$articalId;
+        // }
+        if(is_null($articalId)){
+            $articalId = 0; // Default value if no ID is provided
+        }
       return "Artical ID IS: " .$articalId;
     }
-    function showArticalPage($pageNumber,$type){
-        return 'Artical Page Number IS:'.$pageNumber.'Type IS: '.$type;
+    // function showArticalPage($pageNumber){
+    //      $perPage= request()->input('perPage',10);
+    //      $sort=request()->input('sort','asc');
+    //      return "Page Number is: ".$pageNumber.", perPage:".$perPage.", Sort:".$sort;
+
+    // }
+    function showArticalPage($pageNumber,Request $request){
+         $perPage= $request->input('perPage',10);
+         $sort=$request->input('sort','asc');
+         return "Page Number is: ".$pageNumber.", perPage:".$perPage.", Sort:".$sort;
+
     }
 }
