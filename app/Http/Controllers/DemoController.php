07f1926451ec86c7ffe68bd34189183d9627c600
AkashItlogiko <akashsahanwu089@gmail.com>
2025-06-17 00:57:30 +0600

    p

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class DemoController extends Controller
 {
-    //
+    function  demo1(){
+       return "Response Demo1";
+    }
+    function demo2(){
+        return "Response Demo2";
+    }
+    function demo3(){
+        return "Response Demo3";
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DemoController;
 use App\Http\Controllers\ArticalController;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\DashbordController;
@@ -26,3 +27,6 @@ Route::get('/laravel', function () {
 
 Route::get('/artical/{articalId?}',[ArticalController::class,'index']);
 Route::get('/artical/page/{pageNumber}',[ArticalController::class,'showArticalPage']);
+Route::get('/demo1',[DemoController::class,'demo1']);
+Route::post('/demo2',[DemoController::class,'demo2']);
+Route::put('/demo3',[DemoController::class,'demo3']);
