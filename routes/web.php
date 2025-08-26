813d33523ae6a72656c7ed23aedd02a0daf5c7e5
AkashItlogiko <akashsahanwu089@gmail.com>
2025-08-26 12:28:36 +0600

    ok

--- routes/web.php
+++ routes/web.php
@@ -22,9 +22,7 @@ Route::get("/english",[WelcomeController::class,'englishCourse']);
 Route::get('/dashbord',[DashbordController::class,'index']);
 Route::get('/dashbord/sidebar',[DashbordController::class,'indexTwo']);
 
-// Route::get('/hello', function () {
-//     return('Hello World');
-// });
+ 
 
 Route::get('/laravel', function () {
     return('Hello Laravel');
